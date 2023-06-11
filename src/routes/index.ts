08825d24aaf58ed5fc0aa1cf2b6f65defef0c215
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-11 16:25:20 +1200

    Configured Backend API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
-import {Router} from "express";
-import {getProjects} from "../controllers/projects/ProjectsController";
+import { Router, Response } from "express";
+import { getProjects } from "../controllers/projects/ProjectsController";
 
 const router: Router = Router();
 
