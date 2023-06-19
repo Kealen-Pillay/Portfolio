be1f049e2a2d8f5a7caa879662447ea48d595889
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-19 17:49:41 +1200

    Added hover to skills

--- client/src/components/skills/Skills.tsx
+++ client/src/components/skills/Skills.tsx
@@ -5,7 +5,7 @@ import {
   PythonPlain,
   JavaPlain,
   TypescriptPlain,
-  GoLine,
+  GoPlain,
 } from "devicons-react";
 
 const Skills = () => {
@@ -15,25 +15,25 @@ const Skills = () => {
         <h1 className="text-white font-bold text-5xl mx-5 my-10">Skills</h1>
         <div className="flex flex-col mb-10">
           <div className="flex flex-row space-x-2 mx-5 mb-2">
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50">
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e]">
               <JavaPlain color="#21807c" size="60" />
             </div>
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50">
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e]">
               <PythonPlain color="#21807c" size="60" />
             </div>
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50">
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e]">
               <TypescriptPlain color="#21807c" size="60" />
             </div>
           </div>
           <div className="flex flex-row space-x-2 mx-5">
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50">
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e]">
               <ReactOriginal color="#21807c" size="60" />
             </div>
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50">
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e]">
               <NodejsPlain color="#21807c" size="60" />
             </div>
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50">
-              <GoLine color="#21807c" size="60" />
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e]">
+              <GoPlain color="#21807c" size="60" />
             </div>
           </div>
         </div>
