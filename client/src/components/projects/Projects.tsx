a96c09ecca51376b7b9bbda7b32d94c2b9746a54
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-20 13:44:02 +1200

    Added new start script to server

--- client/src/components/projects/Projects.tsx
+++ client/src/components/projects/Projects.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import {getProjects} from "../../api/API";
 import {IProject} from "../../types/type";
 
+
+
 const Projects = () => {
     const [projectList, setProjects] = useState<IProject[]>([]);
 
@@ -12,7 +14,7 @@ const Projects = () => {
 
     const fetchProjects = (): void => {
         getProjects()
-            .then(({data: {projects}}: IProject[] | any) => console.log(projects)) //TODO: TEST THIS!
+            .then(({data: {projects}}: IProject[] | any) => setProjects(projects))
             .catch((err: Error) => console.log(err));
     };
 
@@ -20,6 +22,9 @@ const Projects = () => {
         <>
             <section className="flex h-screen w-screen Roboto bg-[#1e1e1e]">
                 <h1 className="text-white font-bold text-5xl mx-5 my-10">Projects</h1>
+                <div className="w-screen">
+
+                </div>
             </section>
         </>
     );
