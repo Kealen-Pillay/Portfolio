0b2a8b76e4940e6cdd346913840036484f47823c
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-20 13:52:54 +1200

    mc

--- client/src/components/projects/Projects.tsx
+++ client/src/components/projects/Projects.tsx
@@ -23,7 +23,6 @@ const Projects = () => {
             <section className="flex h-screen w-screen Roboto bg-[#1e1e1e]">
                 <h1 className="text-white font-bold text-5xl mx-5 my-10">Projects</h1>
                 <div className="w-screen">
-
                 </div>
             </section>
         </>
