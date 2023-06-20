e5038c90813f0c7a411eb59c968765385d4ddd9a
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-20 17:11:53 +1200

    Formatted files

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -3,35 +3,35 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
-    const year: number = new Date().getFullYear();
+  const year: number = new Date().getFullYear();
 
-    return (
-        <>
-            <section className="flex flex-row h-10 w-screen bg-[#2e2e2e] items-center justify-between">
-                <div className="flex flex-row px-5">
-                    <a
-                        href="https://www.linkedin.com/in/kealen-pillay-603306200/"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="cursor-pointer"
-                    >
-                        <LinkedInIcon sx={{color: "#ffffff", marginRight: 2}}/>
-                    </a>
-                    <a
-                        href="https://github.com/Kealen-Pillay"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="cursor-pointer"
-                    >
-                        <GitHubIcon sx={{color: "#ffffff"}}/>
-                    </a>
-                </div>
-                <div className="px-5 text-white Roboto">
-                    <h3>Kealen Pillay {year}</h3>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="flex flex-row h-10 w-screen bg-[#2e2e2e] items-center justify-between">
+        <div className="flex flex-row px-5">
+          <a
+            href="https://www.linkedin.com/in/kealen-pillay-603306200/"
+            target="_blank"
+            rel="noreferrer"
+            className="cursor-pointer"
+          >
+            <LinkedInIcon sx={{ color: "#ffffff", marginRight: 2 }} />
+          </a>
+          <a
+            href="https://github.com/Kealen-Pillay"
+            target="_blank"
+            rel="noreferrer"
+            className="cursor-pointer"
+          >
+            <GitHubIcon sx={{ color: "#ffffff" }} />
+          </a>
+        </div>
+        <div className="px-5 text-white Roboto">
+          <h3>Kealen Pillay {year}</h3>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Footer;
