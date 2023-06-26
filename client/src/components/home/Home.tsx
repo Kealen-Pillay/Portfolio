c6a54a79b25d4ee836c4d91d6d7a67c84e5763ac
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-26 20:12:28 +1200

    Added responsiveness to home screen

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -5,10 +5,15 @@ import Typewriter from "typewriter-effect";
 const Home = () => {
   return (
     <>
-      <section className="flex justify-center text-center items-center h-screen w-screen Roboto">
-        <div className="flex flex-col items-start text-white font-bold text-5xl">
-          <h1>Hi,</h1>
-          <h1> My Name Is</h1>
+      <section className="flex justify-center items-center h-screen w-screen Roboto relative">
+        <div className="h-screen absolute">
+          <ParticlesApp />
+        </div>
+        <div className="flex flex-col items-start text-white font-bold text-5xl sm:text-6xl md:text-7xl">
+          <div className="flex flex-col sm:flex-row">
+            <h1 className="pr-2">Hi,</h1>
+            <h1> My Name Is</h1>
+          </div>
           <div className="Roboto text-teal-500 font-bold">
             <Typewriter
               options={{
@@ -17,9 +22,6 @@ const Home = () => {
                 loop: true,
               }}
             />
-          </div>
-          <div>
-            <ParticlesApp />
           </div>
         </div>
       </section>
