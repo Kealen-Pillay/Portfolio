2d1bc25db38f83c249bd7fe7a79a324693678bc8
Kealen-Pillay <kealenpillay@gmail.com>
2023-06-29 16:30:10 +1200

    Centered projects

--- client/src/components/projects/Projects.tsx
+++ client/src/components/projects/Projects.tsx
@@ -44,47 +44,43 @@ const Projects = () => {
   return (
     <>
       <section className="flex flex-col h-screen w-screen Roboto bg-[#1e1e1e]">
-        <h1 className="text-white font-bold text-5xl mx-5 mt-10 mb-5">
-          Projects
-        </h1>
-        <div className="flex items-start justify-center flex-row w-screen h-full bg-teal-500">
-          <div className="overflow-hidden relative">
-            <div className="absolute inset-0 flex items-center justify-between h-screen pb-10">
-              <button onClick={prevSlide} className="rounded-full">
-                <ChevronLeftIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
-              </button>
-              <button onClick={nextSlide} className="rounded-full">
-                <ChevronRightIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
-              </button>
-            </div>
-            <div className="flex flex-col text-white w-screen h-full justify-center pl-6 pr-6 py-5  bg-rose-500">
-              {projectList && (
-                <div
-                  className="transition-transform ease-out duration-500"
-                  key={projectList[index]?._id}
-                >
-                  <h1 className="text-3xl font-bold mb-5">
-                    {projectList[index]?.name}
-                  </h1>
-                  <div className="hidden lg:block mb-5 border rounded overflow-hidden">
-                    <img src={images[index][0]} alt="preview" />
-                  </div>
-                  <div className="flex flex-row items-center w-72 h-12 mb-5">
-                    <LayersIcon />
-                    <h3 className="ml-2 text-sm">
-                      {projectList[index]?.technologies.map(
-                        (technology: string, currentIndex) =>
-                          currentIndex !==
-                          projectList[index]?.technologies.length - 1
-                            ? technology + ", "
-                            : technology
-                      )}
-                    </h3>
-                  </div>
-                  <p>{projectList[index]?.description}</p>
+        <h1 className="text-white font-bold text-5xl mx-5 mt-10">Projects</h1>
+        <div className="flex items-start justify-center flex-row w-screen h-full overflow-hidden relative">
+          <div className="absolute inset-0 flex items-center justify-between">
+            <button onClick={prevSlide} className="rounded-full">
+              <ChevronLeftIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
+            </button>
+            <button onClick={nextSlide} className="rounded-full">
+              <ChevronRightIcon sx={{ color: "#FFFFFF" }} fontSize="large" />
+            </button>
+          </div>
+          <div className="flex flex-col text-white w-screen h-full justify-center px-10 pb-20">
+            {projectList && (
+              <div
+                className="transition-transform ease-out duration-500"
+                key={projectList[index]?._id}
+              >
+                <h1 className="text-4xl font-bold mb-5 sm:text-5xl">
+                  {projectList[index]?.name}
+                </h1>
+                <div className="hidden lg:block mb-5 border rounded overflow-hidden">
+                  <img src={images[index][0]} alt="preview" />
                 </div>
-              )}
-            </div>
+                <div className="flex flex-row items-center w-auto pr-10 mb-5 py-3">
+                  <LayersIcon />
+                  <h3 className="ml-2 font-bold text-sm sm:text-base">
+                    {projectList[index]?.technologies.map(
+                      (technology: string, currentIndex) =>
+                        currentIndex !==
+                        projectList[index]?.technologies.length - 1
+                          ? technology + ", "
+                          : technology
+                    )}
+                  </h3>
+                </div>
+                <p className="sm:text-lg">{projectList[index]?.description}</p>
+              </div>
+            )}
           </div>
         </div>
       </section>
