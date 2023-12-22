1906097a066656a8aa2022c706fbfa907da93e7e
Kealen-Pillay <kealenpillay@gmail.com>
2023-12-22 14:34:21 +1300

    chore: Updated skills

--- client/src/components/skills/Skills.tsx
+++ client/src/components/skills/Skills.tsx
@@ -5,7 +5,8 @@ import {
   PythonPlain,
   JavaPlain,
   TypescriptPlain,
-  GoPlain,
+  NextjsLine,
+  CsharpLine,
 } from "devicons-react";
 
 const Skills = () => {
@@ -18,60 +19,59 @@ const Skills = () => {
         <div className="flex flex-col mb-10 sm:items-center sm:flex-row sm:justify-center sm:mb-14 md:ml-7 md:justify-start">
           <div className="flex flex-row space-x-2 mx-5 mb-2 sm:mx-1 sm:mb-0">
             <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
-              <JavaPlain color="#21807c" size="60" />
-            </div>
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
-              <PythonPlain color="#21807c" size="60" />
-            </div>
-            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
               <TypescriptPlain color="#21807c" size="60" />
+            </div>
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
+              <ReactOriginal color="#21807c" size="60" />
+            </div>
+            <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
+              <NextjsLine color="#21807c" size="60" />
             </div>
           </div>
           <div className="flex flex-row space-x-2 mx-5 sm:mx-1">
             <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
-              <ReactOriginal color="#21807c" size="60" />
+              <PythonPlain color="#21807c" size="60" />
             </div>
             <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
               <NodejsPlain color="#21807c" size="60" />
             </div>
             <div className="flex justify-center items-center h-28 w-28 bg-gray-50 hover:bg-[#1e1e1e] sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36">
-              <GoPlain color="#21807c" size="60" />
+              <CsharpLine color="#21807c" size="60" />
             </div>
           </div>
         </div>
         <div className="block text-white font-bold mx-5 sm:text-lg sm:hidden">
           <p>
-            I prefer using statically-typed languages such as Java or
-            TypeScript. I typically use React with TypeScript and have used
-            these technologies in a professional environment. I have also used
-            Python for academic purposes, in particular machine learning and
-            computer vision. For backend development, I lean towards using
-            NodeJS or Go. I am a fan of using the MERN stack, however, I enjoy
-            exploring new tech stacks as well.
+            I prefer using statically-typed languages such as TypeScript or C#.
+            I typically use Next.js or React and have used both of these
+            technologies in a professional production environment. I have used
+            Python for academic purposes, in particular to develop a machine
+            learning gesture recognition solution. For backend development, I
+            lean towards using Node.js, Java, or .NET. Concerning database
+            interactions, I opt for PostgreSQL and MongoDB.
           </p>
         </div>
         <div className="hidden text-white font-bold mx-5 sm:text-base sm:block md:mx-7 xl:text-xl xl:mr-10 2xl:text-2xl">
           <p className="sm:mb-5 2xl:mb-10">
-            I prefer using statically-typed languages such as Java or
-            TypeScript. I have used Java for many academic applications such as
-            data structures and algorithms and smaller full stack prototypes
-            with ApacheDB and SQL. I typically use React with TypeScript and
-            have used these technologies in a professional environment as well
-            as for most of my personal frontend endeavours and prototyping.
+            I prefer using statically-typed languages such as TypeScript or C#.
+            I use Typescript for both the frontend and backend, alongside
+            Next.js, React, and Node.js. Comparatively, I use C# with the .NET
+            framework. I have used these technologies in a professional
+            production environment, as well as for most of my personal
+            prototyping.
           </p>
           <p className="2xl:mb-10">
-            I have also used Python for academic purposes, in particular machine
-            learning and computer vision. For backend development, I lean
-            towards using NodeJS with Express or Go. Go is a relatively new
-            language that I am learning and enjoying. I am a fan of using the
-            MERN stack, however, I enjoy exploring new tech stacks as well.
+            I have also used Python in machine learning and computer vision
+            applications, such as developing a gesture recognition solution. For
+            backend development, I lean towards using Node.js with Express or
+            .NET. I typically lean towards the MERN stack, however, I have
+            developed applications using the LAMP stack.
           </p>
           <p className="hidden 2xl:block">
             In a professional environment, I have performed full stack
-            development using React with Typescript, Jotai state management
-            library, .NET 6, MongoDB. I have also worked in a professional
-            environment using VueJS, vuex state management, PHP, Laravel, SQL,
-            and MongoDB.
+            development using Typescript, React, Next.js, .NET 6, and Node.js. I
+            have also worked in a professional environment using VueJS, Laravel,
+            SQL, and MongoDB.
           </p>
         </div>
       </section>
